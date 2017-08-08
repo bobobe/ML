62c84052cfb8c5655171dedf09518516534de48b
bobobe <420097885@qq.com>
2017-08-08 23:22:37 +0800

    8.8 11:22pm

--- 8/lw_linRegres.py
+++ 8/lw_linRegres.py
@@ -35,8 +35,46 @@ def figure(X,y,ypredict):
 	ax.scatter(x,y,c = 'red',s = 10);#散点图
 	ax.scatter(x,ypredict,c = 'black',s = 10);
 	p.show();
+
+def squareError(ypredict,y):
+	return (ypredict-y).T*(ypredict-y);
 	
 if __name__ == '__main__':
-			X,y = lr.loadData('/usr/local/py_workspace/machine-learning/data/8/ex0.txt');
-			ypredict = multi_lwlr(X,X,y,0.03);#改变k的大小观察拟合程度，选取最佳的k
-			figure(X,y,ypredict);
+			#X,y = lr.loadData('/usr/local/py_workspace/machine-learning/data/8/ex0.txt');
+			#ypredict = multi_lwlr(X,X,y,1);#改变k的大小观察拟合程度，选取最佳的k
+			#figure(X,y,ypredict);
+						
+			#预测鲍鱼年龄误差
+			#利用训练集测试
+			X,y = lr.loadData('/usr/local/py_workspace/machine-learning/data/8/abalone.txt');
+			ypredict = multi_lwlr(X[0:99],X[0:99],y[0:99],0.1);
+			yerror01= squareError(ypredict,y[0:99]);
+			print yerror01;
+			ypredict = multi_lwlr(X[0:99],X[0:99],y[0:99],1);
+			yerror1= squareError(ypredict,y[0:99]);
+			print yerror1;
+			ypredict = multi_lwlr(X[0:99],X[0:99],y[0:99],10);
+			yerror10= squareError(ypredict,y[0:99]);
+			print yerror10;
+			
+           #在新样本集上测试
+			print'new example:'
+			ypredict = multi_lwlr(X[100:199],X[0:99],y[0:99],0.1);
+			yerror01= squareError(ypredict,y[100:199]);
+			print yerror01;
+			ypredict = multi_lwlr(X[100:199],X[0:99],y[0:99],1);
+			yerror1= squareError(ypredict,y[100:199]);
+			print yerror1;
+			ypredict = multi_lwlr(X[100:199],X[0:99],y[0:99],10);
+			yerror10= squareError(ypredict,y[100:199]);
+			print yerror10;
+			
+			#与线性回归比较
+			print 'standard regression:'
+			w = lr.compW(X[0:99],y[0:99]);
+			ypredict = X[100:199]*w;
+			ystanderror =squareError(ypredict,y[100:199]);
+			print ystanderror;
+			
+			
+			
