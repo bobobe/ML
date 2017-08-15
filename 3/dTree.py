4a6a9b90fd731da5ece7cf6e8bd9d5bc8e2ed58f
bobobe <420097885@qq.com>
2017-08-15 22:26:29 +0800

    8.15 22:26pm

--- 3/dTree.py
+++ 3/dTree.py
@@ -1,6 +1,7 @@
 #encoding:utf-8
 #决策树
 import numpy as np;
+from scipy.stats import mode;
 
 def calcEntro(y):#compute the entropy
 	m,n = np.shape(y);
@@ -17,16 +18,71 @@ def calcEntro(y):#compute the entropy
 	entropy = -entropy;
 	return entropy;
 	
-def splitDataSet(X,index,value):#提取第index维等于value的样本,并去掉第index维
-	newFeature = [];
-	for row in X:
-		if(row[index]==value):
-			reduceFeature = list(row[:index]);
-			reduceFeature.extend(row[index+1:]);
-			newFeature.append(reduceFeature);
-	return np.array(newFeature);
-
-if __name__=='__main__':
-	X = np.array([[1,2],[3,1],[3,2],[5,2],[3,1],[3,2],[1,3],[3,2],[4,1]]);
-	print splitDataSet(X,0,1);
+def splitDataSet(X,y,index,value):#提取第index维等于value的样本,并去掉第index维
+	newFeatureX = [];
+	newFeatureY = [];
+	m,n = np.shape(X);
+	i = 0;
+	for i in range(m):
+		if(X[i,index]==value):
+			reduceFeature = list(X[i,:index]);
+			reduceFeature.extend(list(X[i,index+1:]));
+			newFeatureX.append(reduceFeature);
+			newFeatureY.append(y[i]);
+		i+=1;
+	return np.array(newFeatureX),np.array(newFeatureY);
 	
+def chooseBestFeatureToSplit(X,y):#选择信息增益最大的特征来分割,返回该特征的下标
+	m,n = np.shape(X);
+	if(n==1):#if n == 1,express that X only one feature, so the only choose is 0;
+		return 0;
+	Hy = calcEntro(y);
+	#print 'the total entropy is %lf'% Hy;
+	maxInfoIncre = 0;
+	bestFeature = -1;
+	for i in range(n):
+		featureSet = set([ithFeature for ithFeature in X[:,i]]);
+		Hyx = 0;
+		for j in featureSet:
+			xsplit,ysplit = splitDataSet(X,y,i,j);
+			m1,n1 = np.shape(xsplit);
+			p = m1/float(m);
+			Hyx+=p*calcEntro(ysplit);
+		#print 'the %dth feature infoIncrease is %lf' %(i,Hy-Hyx);
+		if Hy - Hyx >maxInfoIncre:
+			maxInfoIncre = Hy-Hyx;
+			bestFeature = i;
+	return bestFeature;
+	
+def majorityCnt(y):#多数表决
+	return int(mode(y,0)[0]);
+		
+def createTree(X,y,labels):#create the dicide tree(labels is the description of the feature,it is a list)
+	m1,n1 = np.shape(X);
+	m2,n2 = np.shape(y);
+	if list(y).count(y[0]) == m2:
+		return y[0][0]; 
+	if n1==0:
+		return majorityCnt(y);
+	splitIndex = chooseBestFeatureToSplit(X,y);
+	splitFeature = labels[splitIndex];
+	myTree = {splitFeature:{}};
+	del(labels[splitIndex]);
+	featureSet = set([splitValue for splitValue in X[:,splitIndex]]);
+	for v in featureSet:
+		subLabels = labels[:];
+		splitX,splitY = splitDataSet(X,y,splitIndex,v);
+		myTree[splitFeature][v] = createTree(splitX,splitY,subLabels);
+	return myTree;
+	
+	
+if __name__=='__main__':
+	X = np.array([[1,1],[1,1],[1,0],[0,1],[0,1]]);
+	y = np.array([[1],[1],[0],[0],[0]]);
+	'''x1,y1= splitDataSet(X,y,0,1);
+	print x1;
+	print y1;'''
+	#print chooseBestFeatureToSplit(X,y);
+	#print majorityCnt(y);
+	labels = ['no sur','flip'];
+	print createTree(X,y,labels);
